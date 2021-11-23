5205c690990746b0cad78c869f9f36336757f077
kkinge79 <kingerykagan@gmail.com>
2021-11-23 10:22:07 -0600

    add navbar from bootstrap, with about linked to it

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+
 # Create your views here.
+
+def home(request):
+  return HttpResponse('<h1>DayByDay</h1>')
+
+def about(request):
+  return render(request, 'about.html')
