84055ba50475e9e46b60e036dadc3a08931bc3a1
kkinge79 <kingerykagan@gmail.com>
2021-11-23 13:44:15 -0600

    change redirect in views for home

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,26 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>DayByDay</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+
+class Day:
+  def __init__(self, rating, mood, highs, lows, notes ):
+    self.rating = rating
+    self.mood = mood
+    self.highs = highs
+    self.lows = lows
+    self.notes = notes
+
+days = [
+  Day(9, 'happy', 'free coffee', 'lost my favorite pen', 'nothing else to add'),
+  Day(3, 'sad', 'good soup', 'bad drinks', 'nothing to add'),
+  Day(5, 'sad', 'good soup', 'bad drinks', 'nothing to add')
+]
+
+def days_index(request):
+  return render(request, 'days/index.html', {'days': days })
+
