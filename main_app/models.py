c4732c38ccd927b0fabd98cd7a1c6fcf6603d88b
kkinge79 <kingerykagan@gmail.com>
2021-11-23 20:45:03 -0600

    added super user

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Day(models.Model):
+  rating = models.IntegerField()
+  mood = models.CharField(max_length=100)
+  highs = models.CharField(max_length=100)
+  lows = models.CharField(max_length=100)
+  notes = models.CharField(max_length=1000)
+
+  # def __str__(self):
+  #     return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -4,8 +4,23 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+from .models import Day
 
+
+# class Day:
+#   def __init__(self, rating, mood, highs, lows, notes ):
+#     self.rating = rating
+#     self.mood = mood
+#     self.highs = highs
+#     self.lows = lows
+#     self.notes = notes
 # Create your views here.
+
+# days = [
+#   Day(9, 'happy', 'free coffee', 'lost my favorite pen', 'nothing else to add'),
+#   Day(3, 'sad', 'good soup', 'bad drinks', 'nothing to add'),
+#   Day(5, 'sad', 'good soup', 'bad drinks', 'nothing to add')
+# ]
 
 def home(request):
   return render(request, 'home.html')
@@ -14,20 +29,6 @@ def about(request):
   return render(request, 'about.html')
 
 
-class Day:
-  def __init__(self, rating, mood, highs, lows, notes ):
-    self.rating = rating
-    self.mood = mood
-    self.highs = highs
-    self.lows = lows
-    self.notes = notes
-
-days = [
-  Day(9, 'happy', 'free coffee', 'lost my favorite pen', 'nothing else to add'),
-  Day(3, 'sad', 'good soup', 'bad drinks', 'nothing to add'),
-  Day(5, 'sad', 'good soup', 'bad drinks', 'nothing to add')
-]
-
 def days_index(request):
+  days = Day.objects.all()
   return render(request, 'days/index.html', {'days': days })
-
