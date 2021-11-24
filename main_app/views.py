8c85e0827fd3905508ad257c017428a0bc9f6c99
kkinge79 <kingerykagan@gmail.com>
2021-11-23 21:21:13 -0600

    details page works

--- main_app/views.py
+++ main_app/views.py
@@ -2,25 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from django.shortcuts import render
-
 from .models import Day
 
-
-# class Day:
-#   def __init__(self, rating, mood, highs, lows, notes ):
-#     self.rating = rating
-#     self.mood = mood
-#     self.highs = highs
-#     self.lows = lows
-#     self.notes = notes
-# Create your views here.
-
-# days = [
-#   Day(9, 'happy', 'free coffee', 'lost my favorite pen', 'nothing else to add'),
-#   Day(3, 'sad', 'good soup', 'bad drinks', 'nothing to add'),
-#   Day(5, 'sad', 'good soup', 'bad drinks', 'nothing to add')
-# ]
 
 def home(request):
   return render(request, 'home.html')
